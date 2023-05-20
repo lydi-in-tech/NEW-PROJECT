9213505ddd7eee06314552a39fb971ab55ef5060
lydi-in-tech <sedibengpelesane@gmail.com>
2023-05-20 23:08:08 +0200

    make search engine work

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ function formatDate(timestamp) {
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let day = days[date.getDay()];
 
-  return `${hours}:${minutes} ${day}`;
+  return `${day} ${hours}:${minutes}`;
 }
 
 
@@ -20,7 +20,7 @@ function formatDate(timestamp) {
 
 
 function showTemp(response) {
-  console.log(response.data)
+  
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let tempElement = document.querySelector("#temp");
@@ -34,12 +34,27 @@ function showTemp(response) {
   tempElement.innerHTML = Math.round(response.data.temperature.current);
   humidElement.innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
   windElement.innerHTML = `Wind Speed:${Math.round(response.data.wind.speed)}%`
-  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  dateElement.innerHTML = `Last update: ${formatDate(response.data.time * 1000)}`;
   iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
   iconElement.setAttribute("alt", response.data.condition.description)
 }
-let apiKey = "o0c5f69b1t0fad2340ee5f05678ddda3";
-let city = "Paris";
+
+function searchCity(city) {
+  let apiKey = "o0c5f69b1t0fad2340ee5f05678ddda3";
+
 let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
 
-  axios.get(apiUrl).then(showTemp)
+axios.get(apiUrl).then(showTemp);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+searchCity("New York");
+
